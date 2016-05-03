f11e187c29042637ddbb1ab2dca0e96fe0974177
Deepakp404 <deepakp404@gmail.com>
2016-05-03 12:57:54 +0530

    Voting logic

---
+++ JAVA_Session2_Assignment1.java
@@ -0,0 +1,18 @@
+
+public class JAVA_Session2_Assignment1 
+{
+	public static void main(String[] args) 
+	{
+		int age = 15;
+		
+		if(age >= 18)
+		{
+			System.out.println("Yes can Vote");
+		}
+		else
+		{
+			System.out.println("No can't Vote");
+		}
+	}
+
+}
